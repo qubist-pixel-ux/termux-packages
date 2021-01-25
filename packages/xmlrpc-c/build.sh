e33ccf849bd62041a151680c870ef8e69453631a
qubist-pixel-ux <74808402+qubist-pixel-ux@users.noreply.github.com>
2021-01-25 20:47:37 +0530

    Configure based on architecture

--- packages/xmlrpc-c/build.sh
+++ packages/xmlrpc-c/build.sh
@@ -5,8 +5,8 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.51.06
 TERMUX_PKG_SRCURL=https://sourceforge.net/projects/xmlrpc-c/files/Xmlrpc-c%20Super%20Stable/1.51.06/xmlrpc-c-${TERMUX_PKG_VERSION}.tgz
 TERMUX_PKG_SHA256=06dcd87d9c88374559369ffbe83b3139cf41418c1a2d03f20e08808085f89fd0
-TERMUX_PKG_BUILD_DEPENDS="libtool, pkg-config"
-TERMUX_PKG_DEPENDS="curl, openssl"
+TERMUX_PKG_BUILD_DEPENDS="libtool, pkg-config, openssl"
+TERMUX_PKG_DEPENDS="curl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --prefix=$PREFIX
 --mandir=$PREFIX/share/man
@@ -16,3 +16,17 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-wininet-client
 --enable-cplusplus
 "
+
+termux_step_pre_configure() {
+ 	if [ $TERMUX_ARCH = "arm" ]; then
+    TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="--build=armv7l-unknown-linux-gnueabi"
+	elif [ $TERMUX_ARCH = "i686" ]; then
+    TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="--build=i686-unknown-linux-gnu"
+	elif [ $TERMUX_ARCH = "x86_64" ]; then
+    TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="--build=x86_64-unknown-linux-gnu"
+	elif [ $TERMUX_ARCH = "aarch64" ]; then
+    TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="--build=arm64-unknown-linux-gnueabi"
+	else
+		termux_error_exit "Unsupported arch: $TERMUX_ARCH"
+	fi
+}
