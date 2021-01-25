91915eeecab70894701c9ec37ddf0d022f6696dd
qubist-pixel-ux <74808402+qubist-pixel-ux@users.noreply.github.com>
2021-01-25 18:36:28 +0530

    Enable xmlrpc-c for rtorrent

--- packages/rtorrent/build.sh
+++ packages/rtorrent/build.sh
@@ -5,4 +5,9 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.9.8
 TERMUX_PKG_SRCURL=https://github.com/rakshasa/rtorrent/releases/download/v${TERMUX_PKG_VERSION}/rtorrent-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=9edf0304bf142215d3bc85a0771446b6a72d0ad8218efbe184b41e4c9c7542af
-TERMUX_PKG_DEPENDS="libcurl, libtorrent, ncurses"
+TERMUX_PKG_DEPENDS="libcurl, libtorrent, ncurses, xmlrpc-c"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--prefix=$PREFIX
+--with-ncursesw
+--with-xmlrpc-c
+"
